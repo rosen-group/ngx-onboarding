1ea03fdb5360cd520f521d55bb07ff1e812fc7e2
SDohle <sdohle@rosen-group.com>
2018-10-05 10:21:47 +0200

    Run tests & publish results

--- projects/ngx-onboarding/karma.conf.js
+++ projects/ngx-onboarding/karma.conf.js
@@ -10,17 +10,23 @@ module.exports = function (config) {
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage-istanbul-reporter'),
-            require('@angular-devkit/build-angular/plugins/karma')
+            require('@angular-devkit/build-angular/plugins/karma'),
+            require('karma-junit-reporter')
         ],
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
         coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, '../../coverage'),
-            reports: ['html', 'lcovonly'],
+            dir: require('path').join(__dirname, '../../reports/coverage'),
+            reports: ['html', 'lcovonly', 'cobertura'],
             fixWebpackSourcePaths: true
         },
-        reporters: ['progress', 'kjhtml'],
+        reporters: ['progress', 'kjhtml', 'junit'],
+        junitReporter: {
+            outputDir: require('path').join(__dirname, '../../reports'),
+            outputFile: 'junit.xml',
+            useBrowserName: false
+        },
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
