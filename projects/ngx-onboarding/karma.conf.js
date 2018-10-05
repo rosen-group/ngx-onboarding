907a81304421ed912f6987693158b454528593a4
SDohle <sdohle@rosen-group.com>
2018-10-05 09:43:22 +0200

    Add chrome debugging port (fix Lost UI shared context error)

--- projects/ngx-onboarding/karma.conf.js
+++ projects/ngx-onboarding/karma.conf.js
@@ -32,7 +32,9 @@ module.exports = function (config) {
                 base: 'Chrome',
                 flags: [
                     '--headless',
+                    '--disable-gpu',
                     '--no-sandbox',
+                    '--remote-debugging-port=9222'
                 ]
             }
         },
