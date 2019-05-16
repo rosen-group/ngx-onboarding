dbf243fee95636dfb961d132a62931078c853ded
Ramon Klein <rklein@rosen-group.com>
2019-05-16 16:37:26 +0200

    #41 Fix broken e2e test because onboarding is not showing up automatically anymore

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,27 +1,34 @@
-import { AppPage } from './app.po';
+import {AppPage} from './app.po';
 
 describe('workspace-project App', () => {
     let page: AppPage;
 
     beforeAll(() => {
-        if (!page) {
-            page = new AppPage();
-        }
+        page = new AppPage();
         page.clearLocalStorage();
     });
 
-    beforeEach(() => {
-        page = new AppPage();
-    });
-
     it('should display welcome message', () => {
-        page.navigateTo();
+        page.navigateToStart();
         expect(page.getParagraphText()).toEqual('Welcome to ngx-onboarding-app!');
     });
 
-    it('should show onboarding overlay initially and hide it if "Turn Off" button is clicked', () => {
+    it('shows onboarding overlay when onboarding is enabled', (done) => {
+
+        page.getOnBoardingButton().click(); // this the main "onboarding" button
+        // after click we expect a menu with 2 items to be visible
+        expect(page.waitAndCountOnboardingMenuItems()).toEqual(2);
+        // get menu item buttons and click the first
+        page.getOnBoardingMenuButtons().then((btns) => {
+            btns[0].click(); // the first button is "Turn on"
+            expect(page.waitAndCountOnboardingHeader()).toBe(1, 'onboarding header should be visible');
+            done();
+        });
+    });
+
+    it('hides onboarding overlay when onboarding was enabled and hide it if "Turn Off" button is clicked', () => {
         expect(page.waitAndCountOnboardingHeader()).toBe(1, 'onboarding header should be visible');
         page.getTurnOffButton().click();
-        expect(page.countOnboardingHeader()).toBe(0, 'onboarding header should not bevisible');
+        expect(page.countOnboardingHeader()).toBe(0, 'onboarding header should not be visible');
     });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,34 +1,48 @@
-import { browser, by, element, protractor } from 'protractor';
+import {browser, by, element, ElementArrayFinder, ElementFinder, promise as wdpromise, protractor} from 'protractor';
+
 const EC = protractor.ExpectedConditions;
 
 export class AppPage {
-    clearLocalStorage() {
-        this.navigateTo();
-        browser.executeScript('window.localStorage.clear();');
+    clearLocalStorage(): wdpromise.Promise<any> {
+        this.navigateToStart();
+        return browser.executeScript('window.localStorage.clear();');
     }
 
-    navigateTo() {
+    navigateToStart(): wdpromise.Promise<any> {
         return browser.get('/');
     }
 
-    getParagraphText() {
+    getParagraphText(): string {
         return element(by.css('app-root h1')).getText();
     }
 
+    getOnBoardingButton(): ElementFinder {
+        return element(by.css('button.onboarding-button'));
+    }
 
-    getTurnOffButton() {
+    getOnBoardingMenuButtons(): ElementArrayFinder {
+        return element.all(by.css('.mat-menu-content button'));
+    }
+
+    getTurnOffButton(): ElementFinder {
         return element(by.css('button[color="warn"]'));
     }
 
-    waitAndCountOnboardingHeader() {
-        // the onboarding overlay always has small delay so we have to wait a big
+    waitAndCountOnboardingMenuItems(): ElementArrayFinder {
+        browser.wait(EC.visibilityOf(element(by.css('.mat-menu-content'))), 10000);
+        return element.all(by.css('.mat-menu-content button')).count();
+    }
+
+    waitAndCountOnboardingHeader(): ElementArrayFinder {
+        // the onboarding overlay always has small delay so we have to wait a bit
         browser.wait(EC.visibilityOf(element(by.css('.onboarding-header'))), 10000);
         return element.all(by.css('.onboarding-header')).count();
     }
 
-    countOnboardingHeader() {
+    countOnboardingHeader(): wdpromise.Promise<number> {
         // the onboarding overlay always has small delay so we have to wait a big
         return element.all(by.css('.onboarding-header')).count();
     }
+
 
 }
